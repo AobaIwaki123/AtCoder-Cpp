6ac69ba196c73173a6b1bf0a4271f2a415ff7b87
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2023-08-22 10:56:25 +0900

    鏡餅問題を復習

--- AtCoderBeginnersSelection/ABC085B-KagamiMochi.cpp
+++ AtCoderBeginnersSelection/ABC085B-KagamiMochi.cpp
@@ -14,20 +14,53 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 
 
 
-int _main()
-{
-    ll N; cin >> N;
-    vector<ll> D(N);
-    rep(i,N) cin >> D[i];
-    sort(all(D), greater<ll>());
-    ll ans = 0;
-    ll pre = INF;
-    rep(i, N) {
-        if(pre > D[i]){
-            pre = D[i];
-            ans++;
+// int _main()
+// {
+//     ll N; cin >> N;
+//     vector<ll> D(N);
+//     rep(i,N) cin >> D[i];
+//     sort(all(D), greater<ll>());
+//     ll ans = 0;
+//     ll pre = INF;
+//     rep(i, N) {
+//         if(pre > D[i]){
+//             pre = D[i];
+//             ans++;
+//         }
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
+
+int _main(){
+    int N;
+    int d[110];
+    cin >> N;
+    rep(i, N) cin >> d[i];
+
+    int num[110] = {0}; // バケット
+    rep(i, N){
+        num[d[i]]++;
+    }
+    int res = 0;
+    rrep(i, 100){
+        if(num[i]){
+            ++res;
         }
     }
-    cout << ans << endl;
+    cout << res << endl;
     return 0;
 }
+
+// int _main(){
+//     int N;
+//     int d[110];
+//     cin >> N;
+//     rep(i, N)cin >> d[i];
+//     set<int> values;
+//     rep(i, N){
+//         values.insert(d[i]);
+//     }
+//     cout << values.size() << endl;
+//     return 0;
+// }
