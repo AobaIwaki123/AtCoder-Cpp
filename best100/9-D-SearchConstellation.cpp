0f5e7170c25f089ea0f91f974b98ab672e83b0e0
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2023-08-24 20:58:42 +0900

    工夫して全列挙する問題を修了

--- best100/9-D-SearchConstellation.cpp
+++ best100/9-D-SearchConstellation.cpp
@@ -13,9 +13,41 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 
 
-
 int _main()
 {
-    
+    ll M; cin >> M;
+    vector<ll> tx(M), ty(M);
+    rep(i, M) cin >> tx[i] >> ty[i];
+    ll N; cin >> N;
+    vector<ll> ux(N), uy(N);
+    rep(i, N) {
+        cin >> ux[i] >> uy[i];
+    }
+
+    ll ax = -1;
+    ll ay = -1;
+
+    rep(i, N){
+        ll dx = ux[i] - tx[0];
+        ll dy = uy[i] - ty[0];
+        ll cnt = 0;
+        reps(j, 1, M){
+            ll x = tx[j] + dx;
+            ll y = ty[j] + dy;
+            ll ok = false;
+            rep(k, N){
+                if(x == ux[k] && y == uy[k]) ok = true;
+            }
+            if(ok) cnt++;
+        }
+        // cout << cnt << endl;
+        if(cnt == M-1){
+            // cout << ux[i] << " " << uy[i] << endl;
+            ax = dx;
+            ay = dy;
+        }
+    }
+
+    cout << ax << " " << ay << endl;
     return 0;
 }
