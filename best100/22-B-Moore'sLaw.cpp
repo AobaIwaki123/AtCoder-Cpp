35a935b1d34573787c8b00bc9b33c6a91292f351
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2023-08-26 18:33:58 +0900

    二分法を自力で実装した。指数関数の微分の復習をした

--- best100/22-B-Moore'sLaw.cpp
+++ best100/22-B-Moore'sLaw.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std; int _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 typedef long long ll;
-typedef pair<ll, ll> P;
+// typedef pair<ll, ll> P;
 #define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
 #define rep(i, n) reps(i, 0, n)
 #define rrep(i, n) reps(i, 1, n + 1)
@@ -12,27 +12,27 @@ const ll INF = 1LL << 60;
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 
-double a;
-// 計算時間を返す関数
+double P;
 double func(double x){
-    return x + a / pow(2, x / 1.5);
+    return 1 - P / pow(2, x / 1.5) * log(2) / 1.5;
+}
+
+double ans(double x){
+    return x + P / pow(2, x / 1.5);
 }
 
 int _main()
 {
     cout << fixed << setprecision(15);
-    cin >> a;
-
-    double L = 0;
-    double R = 1e9;
-    // 三分探索を行う
-    for(int i = 0; i < 500; i++){
-        double c1 = (L * 2 + R) / 3;
-        double c2 = (L + 2 * R) / 3;
-        // 計算時間の最小値を探索
-        if (func(c1) > func(c2)) L = c1;
-        else R = c2;
+    cin >> P;
+    double l = 0, r = 1e18;
+    double EPS = 1e-10;
+    while(fabs(r - l) > EPS){
+        double mid = (l + r) / 2;
+        // cout << mid << endl;
+        if(func(mid) > 0) r = mid;
+        else l = mid;
     }
-    cout << func(R) << endl;
+    cout << ans(l) << endl;
     return 0;
 }
