dd5324bc8ed00fc51eece16b3b514a300fe306a9
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2023-08-27 21:14:41 +0900

    2分探索の王道手法4つを2-2に分けるを学んだ

--- best100/23-C-Darts.cpp
+++ best100/23-C-Darts.cpp
@@ -16,6 +16,30 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 
 int _main()
 {
-    
+    int n;
+    int M;
+    cin >> n >> M;
+    vector<ll> A(n+1);
+    A[0] = 0LL;
+    for(int i = 1; i <= n; i++){
+        cin >> A[i];
+    }
+    sort(all(A));
+    vector<ll> B;
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= n; j++){
+            ll score = A[i] + A[j];
+            B.push_back(score);
+        }
+    }
+
+    priority_queue<ll> pq;
+    sort(all(B));
+    for(ll i = 0; i < (int)B.size(); i++){
+        ll idx = upper_bound(all(B), M-B[i]) - B.begin();
+        if(M - B[i] < 0) continue;
+        pq.push(B[i] + B[idx - 1]);
+    }
+    cout << pq.top() << endl;
     return 0;
 }
