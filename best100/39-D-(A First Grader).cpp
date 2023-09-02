da6a00700bc14c56a6d594b19c3b969f36a308dc
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2023-09-02 18:52:50 +0900

    2次元DPの復習

---
+++ best100/39-D-(A First Grader).cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std; int _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+const ll INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+ll dp[101][21], n,q[101],sum;
+
+int _main()
+{
+    cin >> n;
+    rep(i, n) cin >> q[i];
+
+    dp[0][q[0]] = 1;
+
+    reps(i, 1, n-1){
+        rep(j, 21){
+            if(j + q[i] <= 20) dp[i][j + q[i]] += dp[i-1][j];
+            if(j - q[i] >= 0) dp[i][j - q[i]] += dp[i-1][j];
+        }
+    }
+
+    cout << dp[n-2][q[n-1]] << endl;
+    return 0;
+}
