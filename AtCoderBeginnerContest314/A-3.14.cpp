e0c8708ceb46415fdfdfce3fc8ed9856e0dd4bb7
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2023-09-02 20:57:22 +0900

    過去問の復習

---
+++ AtCoderBeginnerContest314/A-3.14.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std; int _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+const ll INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+
+
+int _main()
+{
+    string S = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    ll N; cin >> N;
+    rep(i, N+2) cout << S[i];
+    cout << endl;
+    return 0;
+}
---
+++ AtCoderBeginnerContest314/B-Roulette.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std; int _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+const ll INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+
+
+int _main()
+{
+    ll N; cin >> N;
+    ll A[110][40];
+    ll C[110];
+    ll ans[110];
+    rep(i, N){
+        ll c; cin >> c;
+        rep(j, c){
+            cin >> A[i][j];
+        }
+        C[i] = c;
+        ans[i] = INF;
+    }
+    ll X; cin >> X;
+    ll ans_min = INF;
+    ll cnt = 0;
+    rep(i, N){
+        // cout << find(A[i], A[i]+C[i], X) - A[i] << endl;
+        if(find(A[i], A[i]+C[i], X) - A[i] < C[i]){
+            ans[i] = C[i];
+            if(ans_min > C[i]){
+                ans_min = C[i];
+                cnt = 0;
+            }
+            if(ans_min == C[i])cnt++;
+        }
+    }
+    cout << cnt << endl;
+    if(cnt > 0){
+        rep(i, N){
+            if(ans[i] == ans_min) cout << i+1 << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
