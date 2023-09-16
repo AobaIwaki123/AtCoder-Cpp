0a53efd954c1a5765e71315ecf7498200b33755a
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2023-09-16 20:55:37 +0900

    319回の復習

---
+++ AtCoderBeginnerContest319/A-LegendaryPlayers.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std; int _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+const ll INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+
+
+int _main()
+{
+    map<string, int> m = {
+        {"tourist", 3858},
+        {"ksun48", 3679},
+        {"Benq", 3658},
+        {"Um_nik", 3648},
+        {"apiad", 3638},
+        {"Stonefeang" ,3630},
+        {"ecnerwala" ,3613},
+        {"mnbvmar" ,3555},
+        {"newbiedmy", 3516},
+        {"semiexp" ,3481},};
+    string s; cin >> s;
+    cout << m[s] << endl;
+    return 0;
+}
