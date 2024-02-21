8e3e827a5c9112d06f37f52032bd18cda18fcfd7
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-02-21 05:34:08 +0000

    Solve ABC 340 A-C

---
+++ AtCoderBeginnerContest340/B-Append.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std; int _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+const ll INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+
+
+int _main()
+{
+    ll Q;
+    cin >> Q;
+    vector<ll> A;
+    
+    rep(i, Q){
+        ll X, Y;
+        cin >> X >> Y;
+        if(X == 1){
+            A.push_back(Y);
+        }
+        else if (X == 2){
+            cout << A[A.size() - Y] << endl;
+        }
+    }
+    return 0;
+}
---
+++ AtCoderBeginnerContest340/C-DevideandDevide.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std; int _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+const ll INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+map<ll, ll> m;
+
+ll f(ll N){
+    if(N == 1) return 0;
+    if(m.count(N)) return m[N];
+    return m[N] = f(N/2) + f((N+1)/2) + N;
+}
+
+int _main()
+{
+    ll N;
+    cin >> N;
+    cout << f(N) << endl;
+    return 0;
+}
