b687a81a3348be8b4e84f5629a1c3255bb6240d2
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-02-21 04:36:06 +0000

    best100 -42 solve!

--- best100/42-D-SilkRoad).cpp
+++ best100/42-D-SilkRoad).cpp
@@ -12,10 +12,24 @@ const ll INF = 1LL << 60;
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 
-
+// 参考 : https://atcoder.jp/contests/joi2015yo/submissions/50475197
 
 int _main()
 {
-    
+    ll N, M;
+    cin >> N >> M;
+    vector<vector<ll>> dp(N+1, vector<ll> (M+1, 0));
+    vector<ll> D(N), C(M);
+    rep(i, N) cin >> D[i];
+    rep(j, M) cin >> C[j];
+    rrep(i, N) rrep(j, M){
+        if(i > j) continue;
+        if(i == j){
+            dp[i][j] = (ll)D[i-1] * C[i-1] + dp[i-1][j-1];
+            continue;
+        }
+        dp[i][j] = min(dp[i][j-1], (ll)D[i-1] * C[j-1] + dp[i-1][j-1]);
+    }
+    cout << dp[N][M] << endl;
     return 0;
 }
